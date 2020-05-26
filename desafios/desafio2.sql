63a44fa9011dfeef6cbe43d34c304088e9399a18
richardwellerson <richardwellerson@gmail.com>
2020-05-26 13:49:13 -0300

    Desafio 3 feito direito agora

--- desafios/desafio2.sql
+++ desafios/desafio2.sql
@@ -1,8 +1,7 @@
 USE SpotifyClone;
 
 CREATE VIEW estatisticas_musicais AS
-SELECT
-COUNT(id) AS cancoes,
+SELECT COUNT(id) AS cancoes,
 (SELECT COUNT(id) FROM artists) AS artistas,
 (SELECT COUNT(id) FROM albums) AS albuns
 FROM songs;
--- desafios/desafio3.sql
+++ desafios/desafio3.sql
@@ -1,5 +1,6 @@
 USE SpotifyClone;
 
+CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.user_name as usuario, s.song_name as 'nome' FROM users AS u
 INNER JOIN last_played AS l ON u.id = l.user_id
 INNER JOIN songs AS s ON s.id = l.song_id
