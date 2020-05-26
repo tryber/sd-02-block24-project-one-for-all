6b35710ea726c6c4b76b3abbec4f20a55b8b4dbf
richardwellerson <richardwellerson@gmail.com>
2020-05-26 13:33:41 -0300

    Correção desafio 3

--- desafios/desafio3.sql
+++ desafios/desafio3.sql
@@ -1,6 +1,6 @@
 USE SpotifyClone;
 
-SELECT u.user_name, s.song_name AS usuario FROM users AS u
+SELECT u.user_name as usuario, s.song_name as 'nome' FROM users AS u
 INNER JOIN last_played AS l ON u.id = l.user_id
 INNER JOIN songs AS s ON s.id = l.song_id
 ORDER BY u.user_name;
