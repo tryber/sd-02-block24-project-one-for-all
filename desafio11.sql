283ee0dc1c1c576fd24b53f9baf690cda0d21d18
douglas-he <doughrqe@gmail.com>
2020-05-27 00:26:40 -0300

    CC

--- desafio11.sql
+++ desafio11.sql
@@ -2,16 +2,16 @@ USE SpotifyClone;
 
 CREATE VIEW CancoesPremium AS
 SELECT
-	music_name AS nome,
-	COUNT(rh.music_id) AS reproducoes
+  music_name AS nome,
+  COUNT(rh.music_id) AS reproducoes
 FROM
-	reproduction_history AS rh
-	INNER JOIN songs AS s ON s.id = music_id
-	INNER JOIN users AS u ON u.id = rh.user_id
+  reproduction_history AS rh
+  INNER JOIN songs AS s ON s.id = music_id
+  INNER JOIN users AS u ON u.id = rh.user_id
 WHERE
-	u.plan_id BETWEEN 2
-	AND 3
+  u.plan_id BETWEEN 2
+  AND 3
 GROUP BY
-	rh.music_id
+  rh.music_id
 ORDER BY
-	nome;
+  nome;
