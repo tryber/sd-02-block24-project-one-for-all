0a7a03f6b5acb774a0a873b4bfb7f0bb72f2be87
richardwellerson <richardwellerson@gmail.com>
2020-06-01 12:21:05 -0300

    Corrigindo upcase

--- desafios/desafio11.sql
+++ desafios/desafio11.sql
@@ -3,7 +3,7 @@ USE SpotifyClone;
 CREATE VIEW CancoesPremium AS
 SELECT
 s.song_name AS nome,
-count(lp.song_id) as reproducoes
+COUNT(lp.song_id) as reproducoes
 FROM users AS u
 INNER JOIN last_played AS lp ON lp.user_id = u.id
 INNER JOIN songs AS s ON s.id = lp.song_id
