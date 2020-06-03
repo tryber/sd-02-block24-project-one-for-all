52a8c2494dac91a1711b225697192b5c71f386e3
eddyeboy27 <lipe0503@gmail.com>
2020-06-03 00:03:38 -0300

    fix

--- desafios/desafio10.sql
+++ desafios/desafio10.sql
@@ -2,5 +2,3 @@
 músicas que estão presente atualmente no histórico de reprodução de um usuário ou uma usuária.
 Ao receber o código identificador daquele usuário ou daquela usuária, exiba a quantidade de canções em seu histórico de reprodução.
  */
-
- 
