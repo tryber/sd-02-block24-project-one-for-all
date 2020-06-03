e3965ea4cd82be8b06b272988b98e3058a5c0208
eddyeboy27 <lipe0503@gmail.com>
2020-06-03 00:00:57 -0300

    CC ISSUES FIX REQ7

---
+++ desafios/desafio10.sql
@@ -0,0 +1,6 @@
+/* Desafio 10 Crie uma function chamada de quantidade_musicas_no_historico que exibe a quantidade de
+músicas que estão presente atualmente no histórico de reprodução de um usuário ou uma usuária.
+Ao receber o código identificador daquele usuário ou daquela usuária, exiba a quantidade de canções em seu histórico de reprodução.
+ */
+
+ 
--- desafios/desafio7.sql
+++ desafios/desafio7.sql
@@ -1,13 +1,13 @@
 /* Desafio 7 Mostre uma relação de todos os álbuns produzidos por cada artista,
-juntamente com a quantidade de seguidores que aquele artista possui,
-de acordo com os detalhes a seguir.Crie uma VIEW chamada perfil_artistas que,
-na primeira coluna,
-deve exibir a valor identificador do artista,
-com o apelido "artista_id".A segunda coluna deve exibir o nome do artista,
-com o apelido "artista".A terceira coluna deve exibir o valor identificador do álbum,
-com o apelido "album_id".A quarta coluna deve exibir o nome do álbum,
-com o apelido "album".A quinta coluna deve exibir a quantidade de seguidores que aquele artista possui e deve ser apelidada de "seguidores".Seus resultados devem estar ordenados em ordem decrescente,
-baseando - se no número de seguidores. */
+ juntamente com a quantidade de seguidores que aquele artista possui,
+ de acordo com os detalhes a seguir.Crie uma VIEW chamada perfil_artistas que,
+ na primeira coluna,
+ deve exibir a valor identificador do artista,
+ com o apelido "artista_id".A segunda coluna deve exibir o nome do artista,
+ com o apelido "artista".A terceira coluna deve exibir o valor identificador do álbum,
+ com o apelido "album_id".A quarta coluna deve exibir o nome do álbum,
+ com o apelido "album".A quinta coluna deve exibir a quantidade de seguidores que aquele artista possui e deve ser apelidada de "seguidores".Seus resultados devem estar ordenados em ordem decrescente,
+ baseando - se no número de seguidores. */
 
 CREATE VIEW perfil_artistas AS
 SELECT
@@ -18,14 +18,14 @@ SELECT
   (
     SELECT
       COUNT(fo.artist_id)
-	  FROM
+    FROM
       SpotifyClone.followers AS fo
-	  WHERE
+    WHERE
       fo.artist_id = art.id
   ) AS seguidores
 FROM
   SpotifyClone.album_artists AS alar
-INNER JOIN SpotifyClone.artists AS art ON alar.artist_id = art.id
-INNER JOIN SpotifyClone.albuns AS al ON alar.album_id = al.id
+  INNER JOIN SpotifyClone.artists AS art ON alar.artist_id = art.id
+  INNER JOIN SpotifyClone.albuns AS al ON alar.album_id = al.id
 ORDER BY
   seguidores DESC;
