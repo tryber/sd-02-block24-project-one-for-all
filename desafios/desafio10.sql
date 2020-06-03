da8d396fb46eefda536502253f487d6ecf16fad9
eddyeboy27 <lipe0503@gmail.com>
2020-06-03 00:14:44 -0300

    Finalizado req10

--- desafios/desafio10.sql
+++ desafios/desafio10.sql
@@ -2,3 +2,25 @@
 músicas que estão presente atualmente no histórico de reprodução de um usuário ou uma usuária.
 Ao receber o código identificador daquele usuário ou daquela usuária, exiba a quantidade de canções em seu histórico de reprodução.
  */
+
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE qntd INT;
+SELECT
+  COUNT(song_id) AS quantidade_musicas_no_historico
+FROM
+  SpotifyClone.history
+WHERE
+  user_id = id
+INTO qntd;
+RETURN qntd;
+END $$
+
+DELIMITER ;
+
+SELECT quantidade_musicas_no_historico(18);
