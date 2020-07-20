c60f6eea4ba93902ee33107d7ae39ff17afbf96d
Giorgia <giorgialeoniac@gmail.com>
2020-07-19 23:11:12 -0300

    Salvando FEITOS até o 7

---
+++ desafio05.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+
+CREATE VIEW top_2_hits_do_momento AS
+    SELECT 
+        s.music_name AS cancao, COUNT(rh.music_id) AS reproducoes
+    FROM
+        songs AS s
+            INNER JOIN
+        reproduction_history AS rh ON rh.music_id = s.id
+    GROUP BY s.id
+    ORDER BY reproducoes DESC
+    LIMIT 2;
